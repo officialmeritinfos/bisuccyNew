911954658f6fa5a5cd9b9a63e8deb0bee0f05084
Kopium-Net <87044411+officialmeritinfos@users.noreply.github.com>
2023-08-20 20:27:05 +0100

    Added Firebase Key

--- config/larafirebase.php
+++ config/larafirebase.php
@@ -2,6 +2,6 @@
 
 return [
 
-    'authentication_key' => null
+    'authentication_key' => "AAAAuiyFq68:APA91bFenoxb5Ue0vnZF5-zb8BxhISQfya778ssG2J94FilhM3refjXoqeKseDGxOYIOwczpn2-dutzaLAFmQUXLwQ2IdSNuUQu04r78MQ4b-naUDGw2xpHmCFhVkm1kcvF5BsC6pSik"
 
 ];
